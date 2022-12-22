221226bfe031a7c23add6847b0fd2d8d68800022
sburakozdemir <sburakozdemir00@gmail.com>
2022-12-22 23:50:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int time = DateTime.Now.Hour;
+
+ if(time>=6 && time<11){
+Console.WriteLine("Günaydın!");
+}
+else if(time<=18){
+Console.WriteLine("İyi Günler!");
+}
+else
+ {
+Console.WriteLine("İyi geceler!");
+}
+string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+Console.WriteLine(sonuc);
+
